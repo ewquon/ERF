60c88682873755616c5480a0a09a726701274151
Eliot Quon <eliot.quon@nrel.gov>
2025-01-16 23:27:07 -0700

    Cleanup DiffusionSrcForMom_N

--- Source/TimeIntegration/ERF_SlowRhsPre.cpp
+++ Source/TimeIntegration/ERF_SlowRhsPre.cpp
@@ -603,10 +603,8 @@ void erf_slow_rhs_pre (int level, int finest_level,
                                      rho_u_rhs, rho_v_rhs, rho_w_rhs,
                                      tau11, tau22, tau33,
                                      tau12, tau13, tau23,
-                                     tau12_op, tau13_op, tau23_op,
                                      dxInv,
-                                     mf_m, mf_u, mf_v,
-                                     thinbody);
+                                     mf_m, mf_u, mf_v);
             }
         }
 

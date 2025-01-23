975033a303e2b44a83d9b71e1ccebdce8c761df4
Eliot Quon <eliot.quon@nrel.gov>
2025-01-22 22:01:56 -0700

    Correctly save thinbody force and zero out S_rhs
    Previously, the thinbody sources zeroed out the *mom_src MFs, which are not
    actually the complete RHS. While this new implementation has the intedned
    effect, it currently has a tendency to generate acoustic waves.

--- Source/SourceTerms/ERF_AddThinBodySources.cpp
+++ Source/SourceTerms/ERF_AddThinBodySources.cpp
@@ -17,16 +17,12 @@ using namespace amrex;
  * momentum source; when added, this negates the RHS to achieve zero flux.
  *
  * @param[in] level
- * @param[in] xmom_src source terms for x-momentum
- * @param[in] ymom_src source terms for y-momentum
- * @param[in] zmom_src source terms for z-momentum
+ * @param[in] RHS
  * @param[in] thinbody thin immersed body data structure
  */
 
 void add_thin_body_sources (int level,
-                            MultiFab& xmom_src,
-                            MultiFab& ymom_src,
-                            MultiFab& zmom_src,
+                            Vector<MultiFab>& S_rhs,
                             ThinImmersedBody& thinbody)
 {
     BL_PROFILE_REGION("erf_add_thin_body_sources()");
@@ -39,86 +35,26 @@ void add_thin_body_sources (int level,
     // If a thin immersed body is present, add forcing terms
     // *****************************************************************************
     if (l_have_thin_xforce) {
-        MultiFab::Copy(*thinbody.fx[level], xmom_src, 0, 0, 1, 0);
+        MultiFab::Copy(*thinbody.fx[level], S_rhs[IntVars::xmom], 0, 0, 1, 0);
         thinbody.fx[level]->mult(-1., 0, 1, 0);
         ApplyInvertedMask(*thinbody.fx[level], *thinbody.xflux_imask[level], 0);
-        MultiFab::Add(xmom_src, *thinbody.fx[level], 0, 0, 1, 0);
+        //MultiFab::Add(S_rhs[IntVars::xmom], *thinbody.fx[level], 0, 0, 1, 0);
+        ApplyMask(S_rhs[IntVars::xmom], *thinbody.xflux_imask[level], 0);
     }
 
     if (l_have_thin_yforce) {
-        MultiFab::Copy(*thinbody.fy[level], ymom_src, 0, 0, 1, 0);
+        MultiFab::Copy(*thinbody.fy[level], S_rhs[IntVars::ymom], 0, 0, 1, 0);
         thinbody.fy[level]->mult(-1., 0, 1, 0);
         ApplyInvertedMask(*thinbody.fy[level], *thinbody.yflux_imask[level], 0);
-        MultiFab::Add(ymom_src, *thinbody.fy[level], 0, 0, 1, 0);
+        //MultiFab::Add(S_rhs[IntVars::ymom], *thinbody.fy[level], 0, 0, 1, 0);
+        ApplyMask(S_rhs[IntVars::ymom], *thinbody.yflux_imask[level], 0);
     }
 
     if (l_have_thin_zforce) {
-        MultiFab::Copy(*thinbody.fz[level], zmom_src, 0, 0, 1, 0);
+        MultiFab::Copy(*thinbody.fz[level], S_rhs[IntVars::zmom], 0, 0, 1, 0);
         thinbody.fz[level]->mult(-1., 0, 1, 0);
         ApplyInvertedMask(*thinbody.fz[level], *thinbody.zflux_imask[level], 0);
-        MultiFab::Add(zmom_src, *thinbody.fz[level], 0, 0, 1, 0);
+        //MultiFab::Add(S_rhs[IntVars::zmom], *thinbody.fz[level], 0, 0, 1, 0);
+        ApplyMask(S_rhs[IntVars::zmom], *thinbody.zflux_imask[level], 0);
     }
-
-#if 0
-#ifndef AMREX_USE_GPU
-    if (l_have_thin_xforce) {
-        // TODO: Implement particles to better track and output these data
-        if (nrk==2) {
-            for ( MFIter mfi(S_data[IntVars::cons],TileNoZ()); mfi.isValid(); ++mfi)
-            {
-                const Box& tbx = mfi.nodaltilebox(0);
-                const Array4<const Real> & fx = thin_xforce_lev->const_array(mfi);
-                const Array4<const int> & mask = xflux_imask_lev->const_array(mfi);
-                ParallelFor(tbx, [=] AMREX_GPU_DEVICE (int i, int j, int k) {
-                    if (mask(i,j,k)==0) {
-                        amrex::AllPrint() << "thin body fx"<<IntVect(i,j,k)<<" = " << fx(i,j,k) << std::endl;
-                    }
-                });
-            }
-        }
-    }
-#endif
-#endif
-
-#if 0
-#ifndef AMREX_USE_GPU
-    if (l_have_thin_yforce) {
-        // TODO: Implement particles to better track and output these data
-        if (nrk==2) {
-            for ( MFIter mfi(S_data[IntVars::cons],TileNoZ()); mfi.isValid(); ++mfi)
-            {
-                const Box& tby = mfi.nodaltilebox(1);
-                const Array4<const Real> & fy = thin_yforce_lev->const_array(mfi);
-                const Array4<const int> & mask = yflux_imask_lev->const_array(mfi);
-                ParallelFor(tby, [=] AMREX_GPU_DEVICE (int i, int j, int k) {
-                    if (mask(i,j,k)==0) {
-                        amrex::AllPrint() << "thin body fy"<<IntVect(i,j,k)<<" = " << fy(i,j,k) << std::endl;
-                    }
-                });
-            }
-        }
-    }
-#endif
-#endif
-
-#if 0
-#ifndef AMREX_USE_GPU
-    if (l_have_thin_zforce) {
-        // TODO: Implement particles to better track and output these data
-        if (nrk==2) {
-            for ( MFIter mfi(S_data[IntVars::cons],TileNoZ()); mfi.isValid(); ++mfi)
-            {
-                const Box& tbz = mfi.nodaltilebox(2);
-                const Array4<const Real> & fz = thin_zforce_lev->const_array(mfi);
-                const Array4<const int> & mask = zflux_imask_lev->const_array(mfi);
-                ParallelFor(tbz, [=] AMREX_GPU_DEVICE (int i, int j, int k) {
-                    if (mask(i,j,k)==0) {
-                        amrex::AllPrint() << "thin body fz"<<IntVect(i,j,k)<<" = " << fz(i,j,k) << std::endl;
-                    }
-                });
-            }
-        }
-    }
-#endif
-#endif
 }
